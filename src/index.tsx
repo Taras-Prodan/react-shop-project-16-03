20a0e829830891aa379aabaf9f760c13fa204ffd
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-06-30 12:12:17 -0500

    Learning how to create function Declaration

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// const h1 = React.createElement('h1', { id: `title` }, `Hello app`)
-// const p = React.createElement(`p`, null, `Blalalalallalal`)
+function Title() {
+    return <h1>Hello Title Component</h1>
+}
+function Content() {
+    return <p>Blalalallalala</p>
+}
 
-// const app = (
-//     <div>
-//         {h1}
-//         {p}
-//     </div>
-// )
-
-const h1 = <h1 id="title"> Hello App</h1>
-
-const p = (
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo natus
-        eaque ex tenetur soluta quod nisi, suscipit quibusdam rerum beatae!
-    </p>
+const root = ReactDOM.createRoot(document.getElementById(`root`) as HTMLElement)
+root.render(
+    <React.StrictMode>
+        <Title />
+        <Content />
+    </React.StrictMode>
 )
-
-const ul = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-    </ul>
-)
-
-const app = (
-    <div>
-        {h1}
-        {p}
-        {ul}
-    </div>
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
