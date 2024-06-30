acfe5146770e8ddacfa9e6567ca9fd7de730135d
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-06-30 11:56:21 -0500

    learning create JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', { id: `title` }, `Hello app`)
-const p = React.createElement(`p`, null, `Blalalalallalal`)
+// const h1 = React.createElement('h1', { id: `title` }, `Hello app`)
+// const p = React.createElement(`p`, null, `Blalalalallalal`)
+
+// const app = (
+//     <div>
+//         {h1}
+//         {p}
+//     </div>
+// )
+
+const h1 = <h1 id="title"> Hello App</h1>
+
+const p = (
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo natus
+        eaque ex tenetur soluta quod nisi, suscipit quibusdam rerum beatae!
+    </p>
+)
+
+const ul = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
 
 const app = (
     <div>
         {h1}
         {p}
+        {ul}
     </div>
 )
 
