5a5b7a740072709a4c8f7eab6dd30f309ad580d4
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-06-30 12:19:19 -0500

    Added brackets

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom/client'
 //arrow function
 const Title = () => <h1>Hello Title Component</h1>
 
-const Content = () => <p>Blalalallalala</p>
+const Content = () => (
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsum
+        eum, repudiandae assumenda quisquam saepe, laboriosam facilis magni id
+        amet voluptatibus tenetur voluptatum sequi iure!
+    </p>
+)
 
 const root = ReactDOM.createRoot(document.getElementById(`root`) as HTMLElement)
 root.render(
