504d50a1f1d69e9c16c0867d03c3a6f3cee7e411
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-06-30 12:15:16 -0500

    Learning how to write Arrow function

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Title() {
-    return <h1>Hello Title Component</h1>
-}
-function Content() {
-    return <p>Blalalallalala</p>
-}
+//arrow function
+const Title = () => <h1>Hello Title Component</h1>
+
+const Content = () => <p>Blalalallalala</p>
 
 const root = ReactDOM.createRoot(document.getElementById(`root`) as HTMLElement)
 root.render(
