013d89ac30dd4fb5e0c2911b764bbacd0165e042
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-07-07 11:19:41 -0500

    Деструктуризація React

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,14 @@ type ContentType = {
     text1: string
     text2: string
 }
+
+const user = {
+    name: `Jack`,
+    age: 10,
+}
+
+const { name, age } = user
+console.log(name, age)
 const Header = (props: HeaderProps) => {
     return (
         <h1>
