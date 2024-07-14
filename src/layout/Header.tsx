a2ad9f29ec457787cedcff8a9bb0bd02742abfc3
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-07-14 11:05:52 -0500

    Create new folder components + folder Logo and added Logo.tsx in Logo Folder and connect to header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,8 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
-import logo from '../assets/logo.svg'
+import Logo from '../components/Logo/Logo'
 
 type Props = {}
 
@@ -14,22 +11,7 @@ const Header = (props: Props) => {
         <AppBar position="static" sx={{ backgroundColor: 'green' }}>
             <Container>
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        <img src={logo} alt="" />
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Home</Button>
                     <Button color="inherit">Products</Button>
                     <Button color="inherit">Payment</Button>
---
+++ src/layout/Main.tsx
@@ -0,0 +1,6 @@
+type Props = {}
+const Main = (props: Props) => {
+    return <div>Main</div>
+}
+
+export default Main
