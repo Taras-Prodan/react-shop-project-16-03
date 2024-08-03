1c4ea9fed45d64a4c09d58916e43dc8713f1dcd5
Taras-Prodan <taras.prodan.2019@gmail.com>
2024-08-03 10:47:04 -0500

    added hide or show button with hooks

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,6 +1,6 @@
 import { Button, Card, CardContent, Typography, TextField } from '@mui/material'
 import './ProductListItem.css'
-
+import { useState } from 'react'
 type Props = {
     title: string
     description: string
@@ -18,6 +18,21 @@ const ProductListItem = ({
     type,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrementClick = () => {
+        setCount((prevCount) => prevCount + 1)
+    }
+
+    const onDecrementClick = () => {
+        setCount((prevCount) => prevCount - 1)
+    }
+    const [color, setColor] = useState<string>('green')
+
+    const toogleColorChange = () => {
+        setColor((prevState) => (prevState === 'red' ? 'green' : 'red'))
+    }
+
     return (
         <>
             <Typography
@@ -35,12 +50,24 @@ const ProductListItem = ({
                     <h3 className="product-title">{title}</h3>
                     <p className="product-description">{description}</p>
                     <div className="product-features">Type: {type}</div>
+                    <div>
+                        <div>
+                            Color: <span className={`${color}`}>{color}</span>
+                        </div>
+                        <button onClick={toogleColorChange}>
+                            Change color
+                        </button>
+                    </div>
                     <div className="product-capacity">Capacity: {capacity}</div>
                     <div className="product-price"> $ {price}</div>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
-                        <TextField size="small" value="1" />
-                        <Button variant="outlined">+</Button>
+                        <Button variant="outlined" onClick={onDecrementClick}>
+                            -
+                        </Button>
+                        <TextField size="small" value={count} />
+                        <Button variant="outlined" onClick={onIncrementClick}>
+                            +
+                        </Button>
                     </div>
                     <div className="btns-wrapper">
                         <Button variant="outlined">Add to cart</Button>
